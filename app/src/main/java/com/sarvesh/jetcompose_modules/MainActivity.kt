d7a0f31efbe530d560abdf0877a8a8223a0ba7e0
sarvesh.singh <sarvesh.singh@motherson.com>
2023-09-29 10:56:37 +0530

    project setup

---
+++ app/src/main/java/com/sarvesh/jetcompose_modules/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.sarvesh.jetcompose_modules
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.sarvesh.jetcompose_modules.ui.theme.JetCompose_ModulesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetCompose_ModulesTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JetCompose_ModulesTheme {
+        Greeting("Android")
+    }
+}
